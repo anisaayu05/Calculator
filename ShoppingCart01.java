db0773d35bc416f10a4d6050d46c220dd10fffa7
anisaayu05 <127603133+anisaayu05@users.noreply.github.com>
2023-04-06 22:03:55 +0700

    Add files via upload

---
+++ ShoppingCart01.java
@@ -0,0 +1,17 @@
+public class ShoppingCart01 {
+    public static void main(String[] args) {
+        // Declare and initialize String variables
+        String customerName = "Anisa";
+        String item = "sweater";
+        String size = "M";
+        int harga = 47000;
+        int jumlah = 2;
+        String message;
+
+        // Message variable
+        message = customerName + " membeli " + jumlah + " " + item + " ukuran " + size + " " + "dengan total harga: Rp" + (harga + harga) + ".";
+
+        // Print and run the code
+        System.out.println(message);
+    }
+}
