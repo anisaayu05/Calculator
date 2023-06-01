e1f369d81a2a6ca2136a3e83ae7ea466e941ef8b
anisaayu05 <127603133+anisaayu05@users.noreply.github.com>
2023-06-01 09:52:50 +0700

    Add files via upload

---
+++ Countdown.java
@@ -0,0 +1,14 @@
+package Test;
+
+public class Countdown {
+    public static void main (String [] args) {
+        for (int i = 0; i <= 5; i++) {
+            System.out.println(i);
+        }
+        System.out.println("");
+
+        for (int j = 0; j <= 20; j += 2) {
+            System.out.println(j);
+        }
+    }
+}
